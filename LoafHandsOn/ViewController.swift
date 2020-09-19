8bc5f65d77ec4d4e1d4c969c777ffb0c4b6986a2
obaya884 <taku3.884@gmail.com>
2020-09-20 05:56:45 +0900

    Initial Commit

---
+++ LoafHandsOn/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LoafHandsOn
+//
+//  Created by 大林拓実 on 2020/09/20.
+//  Copyright © 2020 Obayashi Takumi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
